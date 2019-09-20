8c0fc51af8c1a6b3f7a8778dde4c8ffe5b9996b0
Tazik Shahjahan <tazikshahjahan@protonmail.com>
2019-09-19 20:12:20 -0400

    Created new branch for less optimized build for profiling

--- detectLetters.cpp
+++ detectLetters.cpp
@@ -39,12 +39,7 @@ void detectLetters(cv::Mat img, std::string img_name, tesseract::TessBaseAPI *ap
         vector<cv::KeyPoint> keypoints;
         detector->detect( img, keypoints);
  
-        cv::Mat img_blurred, img_usm, img_resized;
-        
-        cv::GaussianBlur(img, img_blurred, cv::Size(9, 9), 0, 0, cv::BORDER_DEFAULT);
-        img_usm = 4 * img - 2 * img_blurred;
-        cv::resize(img_usm, img_resized, cv::Size(), 400.0/img.cols, 400.0/img.rows);
-        //cv::imshow("img_resized", img_resized); cv::waitKey(0);
+        //cv::imshow("ROI_resized", ROI_resized); cv::waitKey(0);
         
         for (int i = 0; i < keypoints.size(); i++){
                 float x = keypoints[i].pt.x, y = keypoints[i].pt.y;
@@ -67,34 +62,42 @@ void detectLetters(cv::Mat img, std::string img_name, tesseract::TessBaseAPI *ap
                         bottom = img.rows;
                 
                 cv::Rect bbox = cv::Rect(left, top, right - left, bottom - top);
-                cv::Mat roi = img(bbox);
-                //cv::imshow(img_name + " " + std::to_string(i), roi); cv::waitKey(0);
+                cv::Mat ROI = img(bbox);
+                
+                cv::Mat ROI_blurred, ROI_usm, ROI_resized;
+                
+                cv::GaussianBlur(ROI, ROI_blurred, cv::Size(9, 9), 0, 0, cv::BORDER_DEFAULT);
+                ROI_usm = 4 * ROI - 2 * ROI_blurred;
+                cv::resize(ROI_usm, ROI_resized, cv::Size(), 400.0/ROI.cols, 400.0/ROI.rows);
+                
+                api->SetImage((uchar*)ROI_resized.data, ROI_resized.size().width, ROI_resized.size().height, ROI_resized.channels(), ROI_resized.step1());
+                char* ocr_text = api->GetUTF8Text();
+                std::cout << ocr_text << endl;
+                //cv::imshow(img_name + " " + std::to_string(i), ROI); cv::waitKey(0);
                 //bbox.release();
-                //roi.release();
+                //ROI.release();
         }
         
-        api->SetImage((uchar*)img_resized.data, img_resized.size().width, img_resized.size().height, img_resized.channels(), img_resized.step1());
-        char* ocr_text = api->GetUTF8Text();
-        std::cout << ocr_text << endl;
        
         /*
         cv::Mat img_with_keypoints;
         drawKeypoints( img, keypoints, img_with_keypoints, cv::Scalar(0,0,255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
         cv::imshow("keypoints.jpg", img_with_keypoints ); cv::waitKey(0);
         */
-        
+        /* 
         //OpenCV memory realloc 
         img.release();
         //img_gray.release();
-        img_blurred.release();
-        img_usm.release();
-        img_resized.release();
+        ROI_blurred.release();
+        ROI_usm.release();
+        ROI_resized.release();
         detector.release();
 
         //Tesseract mem realloc
         delete ocr_text;
         ocr_text = nullptr;
         api->Clear();
+        */
 }
 
 int main(int argc,char** argv)
